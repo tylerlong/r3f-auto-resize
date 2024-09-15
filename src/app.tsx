426d9834b14f707d0e00e28e6182257f8e787cc0
Tyler Liu <tyler4long@gmail.com>
2024-09-14 20:55:47 -0700

    Refactor code

--- src/app.tsx
+++ src/app.tsx
@@ -6,19 +6,15 @@ import type { Store } from './store';
 
 const App = auto((props: { store: Store }) => {
   const { store } = props;
-  const planeHeight = 5; // 5 rows in game
+  const { arenaSize } = store;
 
-  // because camera has a 45 degree field of view, we need to calculate the zoom factor (2 ** 0.5)
-  // 0.99 is to show the white border around the plane
-  const zoom = (store.canvasSize.height / planeHeight) * 2 ** 0.5;
-  const planeWidth = ((store.canvasSize.width / store.canvasSize.height) * planeHeight) / 2 ** 0.5;
-
+  console.log('App render');
   return (
     <Canvas>
       <OrbitControls />
       <OrthographicCamera
         makeDefault
-        zoom={zoom}
+        zoom={store.zoom}
         near={5}
         far={50}
         position={[0, 10, 10]}
@@ -27,26 +23,26 @@ const App = auto((props: { store: Store }) => {
       <ambientLight intensity={0.5} />
       <directionalLight position={[50, 50, 50]} intensity={2} />
       <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
-        <planeGeometry args={[planeWidth, planeHeight]} />
+        <planeGeometry args={[arenaSize.width, arenaSize.height]} />
         <meshStandardMaterial color="skyblue" />
       </mesh>
-      <mesh position={[-planeWidth / 2 + 0.5, 0, -2]}>
+      <mesh position={[-arenaSize.width / 2 + 0.5, 0, -2]}>
         <boxGeometry args={[1, 1, 1]} />
         <meshStandardMaterial color="red" />
       </mesh>
-      <mesh position={[-planeWidth / 2 + 0.5, 0, -1]}>
+      <mesh position={[-arenaSize.width / 2 + 0.5, 0, -1]}>
         <boxGeometry args={[1, 1, 1]} />
         <meshStandardMaterial color="green" />
       </mesh>
-      <mesh position={[-planeWidth / 2 + 0.5, 0, 0]}>
+      <mesh position={[-arenaSize.width / 2 + 0.5, 0, 0]}>
         <boxGeometry args={[1, 1, 1]} />
         <meshStandardMaterial color="blue" />
       </mesh>
-      <mesh position={[-planeWidth / 2 + 0.5, 0, 1]}>
+      <mesh position={[-arenaSize.width / 2 + 0.5, 0, 1]}>
         <boxGeometry args={[1, 1, 1]} />
         <meshStandardMaterial color="yellow" />
       </mesh>
-      <mesh position={[-planeWidth / 2 + 0.5, 0, 2]}>
+      <mesh position={[-arenaSize.width / 2 + 0.5, 0, 2]}>
         <boxGeometry args={[1, 1, 1]} />
         <meshStandardMaterial color="purple" />
       </mesh>
--- src/store.ts
+++ src/store.ts
@@ -1,14 +1,31 @@
 import { manage } from 'manate';
+import { debounce } from 'lodash';
+
+const rows = 5; // how many rows in the game
 
 export class Store {
   public canvasSize = { width: window.innerWidth, height: window.innerHeight };
+
+  public get zoom(): number {
+    // because camera has a 45 degree field of view, we need to calculate the zoom factor (2 ** 0.5)
+    return (this.canvasSize.height / rows) * 2 ** 0.5;
+  }
+
+  public get arenaSize(): { width: number; height: number } {
+    const height = rows;
+    const width = this.canvasSize.width / this.zoom;
+    return { width, height };
+  }
 }
 
 const store = manage(new Store());
 
-window.addEventListener('resize', () => {
+// debounce the resize event
+const dHandler = debounce(() => {
   store.canvasSize.width = window.innerWidth;
   store.canvasSize.height = window.innerHeight;
-});
+}, 100);
+
+window.addEventListener('resize', dHandler);
 
 export default store;
