44633283621cbd4b3a37f0b1afdd5a7ac597f135
Tyler Liu <tyler4long@gmail.com>
2024-09-14 20:03:56 -0700

    First working version

--- src/app.tsx
+++ src/app.tsx
@@ -1,48 +1,57 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
+import { OrbitControls, OrthographicCamera } from '@react-three/drei';
+import { auto } from 'manate/react';
+import type { Store } from './store';
 
-import { Box, OrthographicCamera } from '@react-three/drei';
+const App = auto((props: { store: Store }) => {
+  const { store } = props;
+  const planeHeight = 5; // 5 rows in game
 
-function App() {
-  // Define positions for "plants" on the left side
-  const plantPositions: [number, number, number][] = [
-    [-10, 0, 0],
-    [-15, 0, 0],
-    [-10, 0, -5],
-    [-15, 0, -5],
-    [-10, 0, -10],
-    [-15, 0, -10],
-    [-10, 0, 5],
-    [-15, 0, 5],
-    [-10, 0, 10],
-    [-15, 0, 10],
-  ];
+  // because camera has a 45 degree field of view, we need to calculate the zoom factor (2 ** 0.5)
+  // 0.99 is to show the white border around the plane
+  const zoom = (store.canvasSize.height / planeHeight) * 2 ** 0.5;
+  const planeWidth = ((store.canvasSize.width / store.canvasSize.height) * planeHeight) / 2 ** 0.5;
 
   return (
     <Canvas>
-      {/* Camera setup */}
+      <OrbitControls />
       <OrthographicCamera
         makeDefault
-        zoom={30}
-        near={10}
-        far={100}
-        position={[0, 50, 50]}
+        zoom={zoom}
+        near={5}
+        far={50}
+        position={[0, 10, 10]}
         rotation={[-Math.PI / 4, 0, 0]}
       />
       <ambientLight intensity={0.5} />
       <directionalLight position={[50, 50, 50]} intensity={2} />
-
-      {/* Ground grid */}
-      <gridHelper args={[30, 30, 'green', 'blue']} />
-
-      {/* Render boxes as plants */}
-      {plantPositions.map((position, index) => (
-        <Box key={index} position={position}>
-          <meshStandardMaterial attach="material" color="green" />
-        </Box>
-      ))}
+      <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
+        <planeGeometry args={[planeWidth, planeHeight]} />
+        <meshStandardMaterial color="skyblue" />
+      </mesh>
+      <mesh position={[-planeWidth / 2 + 0.5, 0, -2]}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="red" />
+      </mesh>
+      <mesh position={[-planeWidth / 2 + 0.5, 0, -1]}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="green" />
+      </mesh>
+      <mesh position={[-planeWidth / 2 + 0.5, 0, 0]}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="blue" />
+      </mesh>
+      <mesh position={[-planeWidth / 2 + 0.5, 0, 1]}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="yellow" />
+      </mesh>
+      <mesh position={[-planeWidth / 2 + 0.5, 0, 2]}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="purple" />
+      </mesh>
     </Canvas>
   );
-}
+});
 
 export default App;
--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,14 @@
 import { manage } from 'manate';
 
 export class Store {
-  public count = 0;
+  public canvasSize = { width: window.innerWidth, height: window.innerHeight };
 }
 
 const store = manage(new Store());
+
+window.addEventListener('resize', () => {
+  store.canvasSize.width = window.innerWidth;
+  store.canvasSize.height = window.innerHeight;
+});
 
 export default store;
